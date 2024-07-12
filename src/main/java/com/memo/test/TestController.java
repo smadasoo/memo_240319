d9cc847ec8f7bd6cceab573d98faf0a81b5660df
DESKTOP-VS1JCGB\user <test@test.com>
2024-07-12 16:40:28 +0900

    초기환경 세팅 전

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,12 @@
+package com.memo.test;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+	@ResponseBody
+	@
+}
